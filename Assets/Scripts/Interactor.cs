d498d5341fbadc52c038f2a72bf5644fd96ff804
Justin Maintenay <j.maintenay@gmail.com>
2025-07-02 15:45:14 +0200

    ENCULE

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    public Transform InteractorSource;
+    public float InteractRange;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)){
+            Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
+            if(Physics.Raycast(r, out RaycastHit hitInfo, InteractRange))
+            {
+                if(hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+                {
+                    interactObj.Interact();
+                }
+            }
+        }
+    }
+}
+
+public interface IInteractable
+{
+    public void Interact();
+}
